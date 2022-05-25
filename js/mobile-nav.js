6f3c87f198e0a4a4a3472d6a736937cdfe630241
ElanMass <74185697+ElanMass@users.noreply.github.com>
2022-05-25 20:44:29 +0530

    Navigation Added

--- js/mobile-nav.js
+++ js/mobile-nav.js
@@ -18,6 +18,8 @@ button1.addEventListener("click", () => {
 
 var button2 = document.querySelector('.nav-swap');
 var button3 = document.querySelector('.nav-swap1');
+var button4 = document.querySelector('.main-nav-swap1');
+var main_menus = document.querySelector('.main-right-nav');
 var menus = document.querySelector('.right-nav');
 
 button2.addEventListener("click", () => {
@@ -28,19 +30,30 @@ button2.addEventListener("click", () => {
 
 button3.addEventListener("click", () => {
 
-    menus.classList.toggle("right-display");    
+    menus.classList.toggle("right-display");  
 
 });
+
+button4.addEventListener("click", () => {
+    main_menus.classList.toggle("main-right-display");
+    home.classList.remove("main-swap");    
+    contact.classList.remove("main-swap");  
+})
 
 var button_front = document.querySelector('.contact-us');
 var button_ui = document.querySelector('.contact-us-ui');
 var button_back = document.querySelector('.contact-us-back');
 var button_logo = document.querySelector('.contact-us-logo');
 var button_word = document.querySelector('.contact-us-word');
+var main_home_button = document.querySelector('.main-menu-home');
 var home_button = document.querySelector('.menu-home');
+var main_contact_button = document.querySelector('.main-menu-contact');
 var contact_button = document.querySelector('.menu-contact');
 var home = document.querySelector('.main');
 var contact = document.querySelector('.main1');
+var home_name = document.querySelector('.home-hide-nav');
+var contact_name = document.querySelector('.contact-name-hide');
+
 
 
 button_front.addEventListener("click", () => {
@@ -73,16 +86,50 @@ button_word.addEventListener("click", () => {
     contact.classList.toggle("contact-view");
 });
 
+main_home_button.addEventListener("click", () => {
+
+    home.classList.remove("home-hide");
+    home.classList.remove("main-swap");
+    contact.classList.remove("contact-view");
+    main_menus.classList.remove("main-right-display");
+    home_name.classList.remove("contact-name-hide");
+    contact_name.classList.remove("contact-name-view");
+});
+
 home_button.addEventListener("click", () => {
 
     home.classList.remove("home-hide");
+    home.classList.remove("main-swap");
     contact.classList.remove("contact-view");
+    contact.classList.remove("main-swap");
     menus.classList.remove("right-display");
+});
+
+main_contact_button.addEventListener("click", () => {
+
+    home.classList.add("home-hide");
+    contact.classList.add("contact-view");
+    contact.classList.remove("main-swap");
+    main_menus.classList.remove("main-right-display");
+    home_name.classList.add("contact-name-hide");
+    contact_name.classList.add("contact-name-view");
 });
 
 contact_button.addEventListener("click", () => {
 
-    home.classList.toggle("home-hide");
-    contact.classList.toggle("contact-view");
+    home.classList.add("home-hide");
+    home.classList.remove("main-swap");
+    contact.classList.add("contact-view");
+    contact.classList.remove("main-swap");
     menus.classList.remove("right-display");
 });
+
+var button_menu = document.querySelector('.lab-menu');
+
+button_menu.addEventListener("click", () => {
+
+    main_menus.classList.toggle("main-right-display");
+    home.classList.toggle("main-swap");
+    contact.classList.toggle("main-swap");
+
+} )
